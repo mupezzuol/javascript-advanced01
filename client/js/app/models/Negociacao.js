0af3c22f19f6f4ce3f89eec5fbcf989187719048
mupezzuol <murillopezzuol@hotmail.com>
2019-05-14 23:19:37 -0300

    Programacao Defensiva; Copy do atributo Data

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -6,18 +6,18 @@ class Negociacao{
     //this -> é referente a instancia da class
     //A convenção de '_' underline nos atributos significa que o programador NÃO deve modificar os dados, ou seja, dados PRIVATE
     constructor(data, quantidade, valor){
-        this._data = data;
+        //Não é congelado, pois o freeze não é profundo e não congela atributos dentro de outros Objetos, nesse caso Objeto Date
+        this._data = new Date(data.getTime());//Crio nova data a partir da data do parametro, para não alterarem dps.. Isso é chamado de -> 'programação defensiva'
+
         this._quantidade = quantidade;
         this._valor = valor;
         Object.freeze(this);//Congelo essa a instancia criada, para que não seja possivel alterar nossos atributos.. this -> instancias criadas ao decorrer do programa
     }
 
-
-
     //Getter's no padrão JavaScript
     //Posso chamar direto -> obj.quantidade (por debaixo dos panos ele chama dessa forma, fica menos verboso)
     get data(){
-        return this._data;
+        return new Date(this._data.getTime());//Crio uma nova data baseado na data da Classe -> funciona como uma copy -> modo de segurança -> é chamado de 'programação defensiva'
     }
 
     get quantidade(){
