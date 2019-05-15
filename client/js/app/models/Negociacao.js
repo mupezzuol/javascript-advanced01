d0a3ad1e734c333e892479983bdaa3ff3bb78899
mupezzuol <murillopezzuol@hotmail.com>
2019-05-14 22:00:55 -0300

    Criando Class em JavaScrip na pasta Models

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,12 @@
+//JavaScript -> Orientado a Objetos
+class Negociacao{
+
+    //Através do construtor estamos setando os atributos da class
+    constructor(){
+        this.data = new Date();
+        this.quantidade = 1;
+        this.valor = 0.0;
+    }
+
+
+}
