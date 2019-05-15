945090e57661e7bdc7f354202e7dbb3557a76fac
mupezzuol <murillopezzuol@hotmail.com>
2019-05-15 16:50:19 -0300

    Date com Split + MAP

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -16,17 +16,24 @@ class NegociacaoController{
     //Método de ADC
     adiciona(event){
         event.preventDefault();//Não recarrega a página após o submit
-        
-        console.log(typeof this._inputData.value);
 
+        //Utilizo dessa forma para ser criado um Date corretamente do jeito q eu quero... Existem diversas formas
+        // Os '...' significa que cada item do meu array é respectivo para cada parametro, param 1, 2 e 3.
+        //Uso função 'map' para validar meu array, quando ele chegar no indice 1 que é referente ao mês ele fará o 'item -1' pois o Date ele vai de 0 até 11, então o mês 0 é janeiro.
+        //Uso % -> Mod, pois qnd for 1 ele fará o retorno de 1 para subtrair... (0 % 2 = 0), (1 % 2 = 1), (2 % 2 = 0)
+        let data = new Date(...this._inputData
+            .value.split('-')
+            .map(function(item, indice) {
+                return item - indice % 2;
+            }));
 
-        //Criando obj pelo Construtor dele, já passando os valores dos campos
+            //Criando obj pelo Construtor dele, já passando os valores dos campos
         let negociacao = new Negociacao(
-            //Utilizo dessa forma para ser criado um Date corretamente do jeito q eu quero... Existem diversas formas
-            new Date(this._inputData.value.split('-')),//Faço split para quebrar meu Ano, Mês e Dia como um Array, onde a classe Date recebe e cria normalmente
+            data,
             this._inputQuantidade.value,
             this._inputValor.value
         );
+
 
         console.log(negociacao.data);
         console.log(negociacao.quantidade);
