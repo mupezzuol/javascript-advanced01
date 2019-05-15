8dc63d4871298bd94115f78d045b31eb088acf26
mupezzuol <murillopezzuol@hotmail.com>
2019-05-14 21:19:26 -0300

    Ajustes gerais

---
+++ client/js/index.js
@@ -0,0 +1,15 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor')
+];
+
+//Submit do formulário
+document.querySelector('form').addEventListener('submit',function(){
+    
+    var tr = document.createElement('tr');
+    
+
+
+
+});
