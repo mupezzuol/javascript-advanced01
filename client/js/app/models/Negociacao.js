2f7855ad6993e9c955e00885db1d00e9f8f841eb
mupezzuol <murillopezzuol@hotmail.com>
2019-05-14 23:01:33 -0300

    Getter padrão JavaScript

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -4,7 +4,7 @@ class Negociacao{
 
     //Através do construtor estamos setando os atributos da class
     //this -> é referente a instancia da class
-    //A convenção de '_' underline nos atributos significa que o programador NÃO deve modificar os dados
+    //A convenção de '_' underline nos atributos significa que o programador NÃO deve modificar os dados, ou seja, dados PRIVATE
     constructor(data, quantidade, valor){
         this._data = data;
         this._quantidade = quantidade;
@@ -13,20 +13,21 @@ class Negociacao{
 
 
 
-    //Métodos de Acesso get/set
-    getData(){
+    //Getter's no padrão JavaScript
+    //Posso chamar direto -> obj.quantidade (por debaixo dos panos ele chama dessa forma, fica menos verboso)
+    get data(){
         return this._data;
     }
 
-    getQuantidade(){
+    get quantidade(){
         return this._quantidade;
     }
 
-    getValor(){
+    get valor(){
         return this._valor;
     }
 
-    getVolume(){
+    get volume(){
         return this._quantidade * this._valor;
     }
 
