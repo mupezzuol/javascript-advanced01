0709fe94fcaaaadbc085faddb207d057e5e64934
mupezzuol <murillopezzuol@hotmail.com>
2019-05-14 22:56:28 -0300

    Method Get

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -4,15 +4,30 @@ class Negociacao{
 
     //Através do construtor estamos setando os atributos da class
     //this -> é referente a instancia da class
+    //A convenção de '_' underline nos atributos significa que o programador NÃO deve modificar os dados
     constructor(data, quantidade, valor){
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
-    //Método
-    obterVolumer(){
-        return this.quantidade * this.valor;
+
+
+    //Métodos de Acesso get/set
+    getData(){
+        return this._data;
+    }
+
+    getQuantidade(){
+        return this._quantidade;
+    }
+
+    getValor(){
+        return this._valor;
+    }
+
+    getVolume(){
+        return this._quantidade * this._valor;
     }
 
 
