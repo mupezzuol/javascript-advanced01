74cd84544011ead360605ee7c6f1140a4a10735d
mupezzuol <murillopezzuol@hotmail.com>
2019-05-16 18:33:44 -0300

    Blindando Array Negociacoes; Programacao Defensiva

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -12,7 +12,10 @@ class ListaNegociacoes{
 
     //Getter's
     get negociacoes(){
-        return this._negociacoes;
+        //Vamos usar programção defensica para blindarmos nossas negociacoes, para que ninguem consiga fazer nem 'length =0' ou 'push()'
+        //Criamos um array vazio e chamamos o método 'concat' que retorna um novo objeto, como se fosse uma cópia, porém retornamos a lista que o cara quer usando concat
+        //Retornamos somente uma cópia pro client, o veradeiro permanece intacto
+        return [].concat(this._negociacoes);
     }
 
 }
