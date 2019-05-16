a1d538e089a2de02bd82c62227c1522966840a86
mupezzuol <murillopezzuol@hotmail.com>
2019-05-16 17:37:13 -0300

    Aplicando REGEX na Data;

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -15,12 +15,20 @@ class DateHelper{
     }
     
 
+    
     //Utilizo dessa forma para ser criado um Date corretamente do jeito q eu quero... Existem diversas formas
     // Os '...' significa que cada item do meu array é respectivo para cada parametro, param 1, 2 e 3.
     //Uso função 'map' para validar meu array, quando ele chegar no indice 1 que é referente ao mês ele fará o 'item -1' pois o Date ele vai de 0 até 11, então o mês 0 é janeiro.
     //Uso % -> Mod, pois qnd for 1 ele fará o retorno de 1 para subtrair... (0 % 2 = 0), (1 % 2 = 1), (2 % 2 = 0)
     //Arrow Function -> Função em Flecha.. menos verboso, não preciso de  {} para uma instrução, nem do 'return'
     static textoParaData(texto){
+
+        //Valido o formado do texto, se NÃO for igual a REGEX abaixo ele apresenta erro
+        //REGEX formato válido
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+
+
         return new Date(...texto
                 .split('-')
                 .map((item, indice) => item - indice % 2)
