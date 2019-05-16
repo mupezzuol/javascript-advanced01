e156adac3b54dcf55c45cdfad2ecd06827c71e59
mupezzuol <murillopezzuol@hotmail.com>
2019-05-16 20:49:17 -0300

    IIFE; Function auto-chamada;Calculando Volume; Usando forEah;

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -16,6 +16,7 @@ class NegociacoesView{
     //Usamos Arrow Function com MAP para percorrermos nosso model que é nossa lista de negociacoes
     //para cada elemento da lista nós criamos uma TR, no final retorno uma STRING para nosso template para ele renderizar tudo
     //porém temos q juntar todos os TR em uma String só, para isso usamos o 'join()' que retorna tudo em uma string gigante unica, assim retorna todas as TR já
+    //Para o TOTAL usamos o IIFE -> Criamos uma função e chamamos ela ao mesmo tempo, auto-chamada, para podermos colocar mais codigos, trab com var etc..
     _template(model){
         return `
             <table class="table table-hover table-bordered">
@@ -37,11 +38,21 @@ class NegociacoesView{
                             <td>${n.valor}</td>
                             <td>${n.volume}</td>
                         </tr>
-                        
+
                     `).join('')}
                 </tbody>
                 
                 <tfoot>
+                    <td colspan="3"></td>
+                    <td>
+                        ${
+                            (function(){
+                                let total = 0.0;
+                                model.negociacoes.forEach(n => total += n.volume);
+                                return total; //Chamamos a função que retorna o total, auto-chamada -> IIFE
+                            })() //Deve-se usar dessa forma para usar o IIFE(função com auto-chamada) -> (função)()
+                        }
+                    </td>
                 </tfoot>
             </table>
         `;
