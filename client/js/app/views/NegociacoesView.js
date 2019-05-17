44aaf6c647565570d613675d5ed4643b91808d84
mupezzuol <murillopezzuol@hotmail.com>
2019-05-16 21:13:19 -0300

    Arrow Function com Funcional; Explicacoes

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -44,19 +44,45 @@ class NegociacoesView{
                 
                 <tfoot>
                     <td colspan="3"></td>
-                    <td>
-                        ${
-                            (function(){
-                                let total = 0.0;
-                                model.negociacoes.forEach(n => total += n.volume);
-                                return total; //Chamamos a função que retorna o total, auto-chamada -> IIFE
-                            })() //Deve-se usar dessa forma para usar o IIFE(função com auto-chamada) -> (função)()
-                        }
-                    </td>
+                    <td>${ model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}</td>
                 </tfoot>
             </table>
         `;
     }
 
+
+    /*
+    FORMAS DE CALCULAR O TOTAL:
+
+-> Reduce -> Faz o processo de reduzir e retorna somente 1 único valor, usando arrow function é feito o calculo...
+-> Reduce -> Recebe primeiro paramentro 'arrow function' e o segundo o valor que será inicializado no primeiro parametro da function chamada
+
+
+-> Usando REDUCE + ARROW FUNCTION REDUZIDA
+- > (total,n) -> é uma arrow function, 'total' é o valor para ser usado pra soma e o 'n' é a var que representa o item da lista
+${ model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
+
+
+
+-> Usando IIFE + Arrow Function
+
+    ${
+        (function(){
+            let total = 0.0;
+            model.negociacoes.forEach(n => total += n.volume);
+            return total;
+        )()
+    }
+
+
+
+-> Usando Arrow Function + Reduce
+${model.negociacoes.reduce(function(total, n) {
+               return total + n.volume;
+         }, 0.0)
+        }
+    
+    
+    */
 
 }
