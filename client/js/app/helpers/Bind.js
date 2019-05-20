7c6fea7f099c2bd6404d9592472430b990d81fb6
mupezzuol <murillopezzuol@hotmail.com>
2019-05-20 18:17:59 -0300

    Ajustes e comentarios

--- client/js/app/helpers/Bind.js
+++ client/js/app/helpers/Bind.js
@@ -1,5 +1,6 @@
 class Bind{
 
+    //... -> aceita vários valores como parametro e ele irá encapsular dentro de um Array, é como se fosse um VARARGS em Java
     constructor(model, view, ...props) {
 
         let proxy = ProxyFactory.create(model, props, model => {
