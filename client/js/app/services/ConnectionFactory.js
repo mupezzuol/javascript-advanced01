35883d62284463a555ff5447857787ec5330c741
mupezzuol <murillopezzuol@hotmail.com>
2019-05-21 20:52:46 -0300

    Module Pattern; Função anonima no ConnectionFactory

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,44 +1,48 @@
-//Vars de conexão
-var stores = ['negociacoes'];
-var version = 2;
-var dbName = 'jsavancado';
+//Padrão -> Module Pattern
+var ConnectionFactory = (function () {
 
-class ConnectionFactory {
+    //Vars de conexão, ngm acessa, pois a função anoniam retorna somente a classe
+    var stores = ['negociacoes'];
+    var version = 2;
+    var dbName = 'jsavancado';
 
-    //Faço com que não seja possivel instanciar essa classe
-    constructor() {
-        throw new Error('Não é possível criar instâncias de ConnectionFactory');
+    return class ConnectionFactory {
+        //Faço com que não seja possivel instanciar essa classe
+        constructor() {
+            throw new Error('Não é possível criar instâncias de ConnectionFactory');
+        }
+
+
+        static getConnection() {
+
+            return new Promise((resolve, reject) => {
+
+                let openRequest = window.indexedDB.open(dbName, version);
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result);
+                };
+
+                openRequest.onsuccess = e => {
+                    resolve(e.target.result);
+                };
+
+                openRequest.onerror = e => {
+                    console.log(e.target.error);
+                    reject(e.target.error.name);
+                };
+            });
+        }
+
+
+        static _createStores(connection) {
+            stores.forEach(store => {
+                if (connection.objectStoreNames.contains(store)) {
+                    connection.deleteObjectStore(store);
+                }
+                connection.createObjectStore(store, { autoIncrement: true });
+            });
+        }
+
+
     }
-
-
-    static getConnection() {
-
-        return new Promise((resolve, reject) => {
-
-            let openRequest = window.indexedDB.open(dbName, version);
-            openRequest.onupgradeneeded = e => {
-                ConnectionFactory._createStores(e.target.result);
-            };
-
-            openRequest.onsuccess = e => {
-                resolve(e.target.result);
-            };
-
-            openRequest.onerror = e => {
-                console.log(e.target.error);
-                reject(e.target.error.name);
-            };
-        });
-    }
-
-
-    static _createStores(connection) {
-        stores.forEach(store => {
-            if (connection.objectStoreNames.contains(store)) {
-                connection.deleteObjectStore(store);
-            }
-            connection.createObjectStore(store, { autoIncrement: true });
-        });
-    }
-
-}
+})();
