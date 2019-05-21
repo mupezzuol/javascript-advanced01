6b6076d16bfed93293341fc3c5665d971248a09e
mupezzuol <murillopezzuol@hotmail.com>
2019-05-21 20:44:03 -0300

    implementado os metodos

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -12,19 +12,33 @@ class ConnectionFactory {
 
 
     static getConnection() {
-        return new Promise((resolve, reject) => {
-            let openRequest = window.indexedDB.open(dbName, version);
 
+        return new Promise((resolve, reject) => {
+
+            let openRequest = window.indexedDB.open(dbName, version);
             openRequest.onupgradeneeded = e => {
+                ConnectionFactory._createStores(e.target.result);
             };
 
             openRequest.onsuccess = e => {
+                resolve(e.target.result);
             };
 
             openRequest.onerror = e => {
+                console.log(e.target.error);
+                reject(e.target.error.name);
             };
         });
     }
 
+
+    static _createStores(connection) {
+        stores.forEach(store => {
+            if (connection.objectStoreNames.contains(store)) {
+                connection.deleteObjectStore(store);
+            }
+            connection.createObjectStore(store, { autoIncrement: true });
+        });
+    }
 
 }
